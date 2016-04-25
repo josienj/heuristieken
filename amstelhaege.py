bafef573c844d0ad33f567764b29ef23424e2675
TFCVDMEIJ <tfc.vandermeij@gmail.com>
2016-04-25 22:19:21 +0200

    25-4

--- amstelhaege.py
+++ amstelhaege.py
@@ -1,18 +1,22 @@
 import random
 
+#
 class House(object):
     def __init__(self, width, length, free):
         self.width = width
         self.length = length
         self.free = free
-        self.map = Map()
+        map = Map()
         self.position = map.getRandom(self.width, self.length, self.free)
         self.pos_X = self.position[0]
+        print self.position[0]
         self.pos_Y = self.position[1]
+        print self.position[1]
 
 class Position(object):
     def __init__(self, width, length, map):
         position = map.getRandom(width, length)
+        print position + "in init in pos"
 
 class Map(object):
     def __init__(self):
@@ -22,25 +26,31 @@ class Map(object):
     def getRandom(self, width, length, free):
         x = random.uniform((0 + free), (self.x_axis - (width + free)))
         y = random.uniform((0 + free), (self.y_axis - (length + free)))
+        print (x, y)
         return (x, y)
 
 def run(numhouses):
 
     house = []
+    print house
+    i = 0
     for i in range(numhouses):
         while i < (numhouses * 0.6):
-            house[i].append(House(8, 8, 2))
+            house.append(House(8, 8, 2))
+            house[i].position
+            print i, "in 1st while"
+            i += 1
         while i >= (numhouses * 0.6) and i < (numhouses * 0.85):
-            house[i].append(House(10, 7.5, 3))
-        while i >= (numhouses * 0.85):
-            house[i].append(House(11, 10.5, 6))
+            house.append(House(10, 7.5, 3))
+            house[i].position
+            print i, "in 2nd while"
+            i += 1
+        while i >= (numhouses * 0.85) and i < numhouses:
+            house.append(House(11, 10.5, 6))
+            house[i].position
+            print i, "in 3rd while"
+            i += 1
 
+    return house
 
-
-
-
-
-
-
-
-
+run = run(20)
