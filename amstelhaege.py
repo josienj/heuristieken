5239f4be0376251e58e9e8d47173e77861968601
TFCVDMEIJ <tfc.vandermeij@gmail.com>
2016-04-26 13:11:23 +0200

    26-4

--- amstelhaege.py
+++ amstelhaege.py
@@ -1,6 +1,5 @@
 import random
 
-#
 class House(object):
     def __init__(self, width, length, free):
         self.width = width
@@ -9,9 +8,9 @@ class House(object):
         map = Map()
         self.position = map.getRandom(self.width, self.length, self.free)
         self.pos_X = self.position[0]
-        print self.position[0]
+        print "X in House = ", self.position[0]
         self.pos_Y = self.position[1]
-        print self.position[1]
+        print "Y in House = ", self.position[1]
 
 class Position(object):
     def __init__(self, width, length, map):
@@ -26,31 +25,36 @@ class Map(object):
     def getRandom(self, width, length, free):
         x = random.uniform((0 + free), (self.x_axis - (width + free)))
         y = random.uniform((0 + free), (self.y_axis - (length + free)))
-        print (x, y)
+        print "(x, y) in getRandom = ", (x, y)
         return (x, y)
+
+def placeHouses(numhouses):
+
+    houses = []
+    print houses
+    i = 0
+    while i < (numhouses * 0.6):
+        print i, "in 1st while"
+        houses.append(House(8, 8, 2))
+        print "position in run = ", houses[i].position
+        i += 1
+    while i >= (numhouses * 0.6) and i < (numhouses * 0.85):
+        print i, "in 2nd while"
+        houses.append(House(10, 7.5, 3))
+        print "position in run = ", houses[i].position
+        i += 1
+    while i >= (numhouses * 0.85) and i < numhouses:
+        print i, "in 3rd while"
+        houses.append(House(11, 10.5, 6))
+        print "position in run = ", houses[i].position
+        i += 1
+    return houses
 
 def run(numhouses):
 
-    house = []
-    print house
-    i = 0
-    for i in range(numhouses):
-        while i < (numhouses * 0.6):
-            house.append(House(8, 8, 2))
-            house[i].position
-            print i, "in 1st while"
-            i += 1
-        while i >= (numhouses * 0.6) and i < (numhouses * 0.85):
-            house.append(House(10, 7.5, 3))
-            house[i].position
-            print i, "in 2nd while"
-            i += 1
-        while i >= (numhouses * 0.85) and i < numhouses:
-            house.append(House(11, 10.5, 6))
-            house[i].position
-            print i, "in 3rd while"
-            i += 1
-
-    return house
+    print "before placement"
+    houses = placeHouses(numhouses)
+    print houses
+    print "after placement"
 
 run = run(20)
