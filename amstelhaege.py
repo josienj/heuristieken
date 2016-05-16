4b00714926b197841f075085bbc1f2853c9a4638
TFCVDMEIJ <tfc.vandermeij@gmail.com>
2016-05-16 22:14:23 +0200

    16-5

--- amstelhaege.py
+++ amstelhaege.py
@@ -4,30 +4,30 @@ import os.path
 import csv
 
 from Parameters import *
-
 # T = c / log(1+iteraties)
 
-screen = pygame.display.set_mode((MAP_X * PAR, MAP_Y * PAR), dflags)
-screen.fill(WHITE)
-pygame.display.update()
 
-SAVE_PATH_PNG = 'C:\Users\Tom\PycharmProjects\heuristieken\Images'
-SAVE_PATH_CSV = 'C:\Users\Tom\PycharmProjects\heuristieken\CSV'
-
-
-def run(numhouses, runs, attempts):
+def run(numhouses, numwaters, runs, attempts):
     screen.fill(GREEN)
     pygame.display.update()
+    mapwater = Map()
+    print "NUMWATER =", NUM_WATER
+    waters = placewater(numwaters, mapwater)
     houses = placehouses(numhouses)
+    for i in range(numwaters):
+        pygame.draw.rect(screen, waters[i].color, waters[i].rect, 0)
+    pygame.display.update()
     for i in range(numhouses):
         pygame.draw.rect(screen, houses[i].color, houses[i].rect, 0)
     pygame.display.update()
     for j in range(runs):
+        for i in range(numwaters):
+            pygame.draw.rect(screen, waters[i].color, waters[i].rect, 0)
         for k in range(numhouses):
             pygame.draw.rect(screen, GREEN, houses[k].rect, 0)
             houses = replacehouse(houses[k], houses, numhouses, k)
             getcoordinates(houses[k])
-            houses[k].rect = (houses[k].pos_X_L * PAR, houses[k].pos_Y_O * PAR,
+            houses[k].rect = (houses[k].pos_x_l * PAR, houses[k].pos_y_l * PAR,
                               houses[k].width * PAR, houses[k].length * PAR)
             pygame.draw.rect(screen, houses[k].color, houses[k].rect, 0)
             pygame.display.update()
@@ -36,7 +36,7 @@ def run(numhouses, runs, attempts):
     pygame.image.save(screen, 'output.png')
     totalvalue = calculatevalue(houses, numhouses)
     totaldistance = calculatedistance(houses, numhouses)
-    save(numhouses, runs, totalvalue, totaldistance, screen)
+    save(numhouses, numwaters, houses, waters, runs, totalvalue, totaldistance, screen)
 
 
 #####
@@ -53,26 +53,67 @@ class House(object):
         self.free = free
         self.map = Map()
         self.position = self.map.getrandom(self.width, self.length, self.free)
-        self.pos_X_L = self.position[0]
-        self.pos_X_R = self.position[0] + self.width
-        self.pos_Y_O = self.position[1]
-        self.pos_Y_B = self.position[1] + self.length
-        listlength = len(houses)
+        self.pos_x_l = 0
+        self.pos_x_r = 0
+        self.pos_y_l = 0
+        self.pos_y_u = 0
         getcoordinates(self)
-        if listlength != 0:
-            for i in range(listlength):
+        currenthouses = len(houses)
+        if currenthouses != 0:
+            for i in range(currenthouses):
                 while checkoverlap(self, houses[i], checkdistance(self, houses[i])):
                     self.position = self.map.getrandom(self.width, self.length, self.free)
                     getcoordinates(self)
-        self.rect = (self.pos_X_L * PAR, self.pos_Y_O * PAR,
+        self.rect = (self.pos_x_l * PAR, self.pos_y_l * PAR,
                      self.width * PAR, self.length * PAR)
         self.color = GREEN
+
+
+class Water(object):
+    def __init__(self, mapwater):
+        self.surface = 0
+        if mapwater.waterbodies == 0:
+            print "totalwater of map =", mapwater.totalwater
+            print "remainingsurface of map =", mapwater.remainingsurface
+        if mapwater.waterbodies < (NUM_WATER - 1):
+            self.surface = random.uniform(0, mapwater.remainingsurface)
+        else:
+            self.surface = mapwater.remainingsurface
+        print "surface =", self.surface
+        mapwater.remainingsurface -= self.surface
+        print "remainingsurface =", mapwater.remainingsurface
+        mapwater.waterbodies += 1
+        print "waterbodies =", mapwater.waterbodies
+        rand = random.randint(0, 1)
+        print "random =", rand
+        if rand == 0:
+            self.width = random.uniform(math.sqrt(self.surface), (2 * math.sqrt(self.surface)))
+            self.length = self.surface / self.width
+        if rand == 1:
+            self.length = random.uniform(math.sqrt(self.surface), (2 * math.sqrt(self.surface)))
+            self.width = self.surface / self.length
+        print "length =", self.length
+        print "width =", self.width
+        self.position = mapwater.getrandom(self.width, self.length, 0)
+        print "position =", self.position
+        self.pos_x_l = 0
+        self.pos_x_r = 0
+        self.pos_y_l = 0
+        self.pos_y_u = 0
+        getcoordinateswater(self)
+        self.rect = ((self.pos_x_l * PAR), (self.pos_y_l * PAR),
+                     (self.width * PAR), (self.length * PAR))
+        self.color = BLUE
 
 
 class Map(object):
     def __init__(self):
         self.x_axis = MAP_X
         self.y_axis = MAP_Y
+
+        self.totalwater = (MAP_X * MAP_Y) * 0.2
+        self.waterbodies = 0
+        self.remainingsurface = self.totalwater
 
     def getrandom(self, width, length, free):
         x = random.uniform((0 + free), (self.x_axis - (width + free)))
@@ -81,13 +122,13 @@ class Map(object):
 
 
 def calculatemin_distance(house, houses, numhouses, housenumber):
-    min_distance = house.pos_X_L
-    if house.map.x_axis - house.pos_X_R < min_distance:
-        min_distance = house.map.x_axis - house.pos_X_R
-    if house.pos_Y_O < min_distance:
-        min_distance = house.pos_Y_O
-    if house.map.y_axis - house.pos_Y_B < min_distance:
-        min_distance = house.map.y_axis - house.pos_Y_B
+    min_distance = house.pos_x_l
+    if house.map.x_axis - house.pos_x_r < min_distance:
+        min_distance = house.map.x_axis - house.pos_x_r
+    if house.pos_y_l < min_distance:
+        min_distance = house.pos_y_l
+    if house.map.y_axis - house.pos_y_u < min_distance:
+        min_distance = house.map.y_axis - house.pos_y_u
     for i in range(numhouses):
         if i == housenumber:
             continue
@@ -126,72 +167,79 @@ def checkoverlap(house, checkedhouse, distance):
 
 
 def checkdistance(house, housechecked):
-    XL1 = house.pos_X_L
-    XR1 = house.pos_X_R
-    YO1 = house.pos_Y_O
-    YB1 = house.pos_Y_B
-    XL2 = housechecked.pos_X_L
-    XR2 = housechecked.pos_X_R
-    YO2 = housechecked.pos_Y_O
-    YB2 = housechecked.pos_Y_B
+    x1l = house.pos_x_l
+    x1r = house.pos_x_r
+    y1l = house.pos_y_l
+    y1u = house.pos_y_u
+    x2l = housechecked.pos_x_l
+    x2r = housechecked.pos_x_r
+    y2l = housechecked.pos_y_l
+    y2u = housechecked.pos_y_u
 
     dx = 0
     dy = 0
     distance = 0
 
-    if XL1 == XL2:
+    if x1l == x2l:
         distance = 0
 
     # If house and house[i] overlap on x-axis
-    elif XL2 < XL1 < XR2 or XL2 > XL1 and XL2 < XR1:
+    elif x2l < x1l < x2r or x2l > x1l and x2l < x1r:
         # If house is lower than house[i]
-        if YO1 < YO2:
-            distance = YO2 - YB1
+        if y1l < y2l:
+            distance = y2l - y1u
         # If house is higher than house[i]
-        elif YO1 > YO2:
-            distance = YO1 - YB2
+        elif y1l > y2l:
+            distance = y1l - y2u
 
     # If house and house[i] overlap on y-axis
-    elif YO2 < YO1 < YB2 or YO2 > YO1 and YO2 < YB1:
+    elif y2l < y1l < y2u or y2l > y1l and y2l < y1u:
         # If house is lower than house[i]
-        if XL1 > XL2:
-            distance = XL1 - XR2
+        if x1l > x2l:
+            distance = x1l - x2r
         # If house is higher than house[i]
-        elif XL1 < XL2:
-            distance = XL2 - XR1
+        elif x1l < x2l:
+            distance = x2l - x1r
 
     # If house is left of house[i]
-    elif XL2 > XL1 and XL2 >= XR1:
+    elif x2l > x1l and x2l >= x1r:
         # If house is lower than house[i]
-        if YO2 > YO1:
-            dy = YO2 - YB1
-            dx = XL2 - XR1
+        if y2l > y1l:
+            dy = y2l - y1u
+            dx = x2l - x1r
         # if house is higher than house[i]
-        elif YO2 < YO1:
-            dy = YO1 - YB2
-            dx = XL2 - XR1
+        elif y2l < y1l:
+            dy = y1l - y2u
+            dx = x2l - x1r
         distance = math.sqrt(math.pow(dx, 2) + math.pow(dy, 2))
 
     # If house is right of house[i]
-    elif XL2 < XL1 and XR2 <= XL1:
+    elif x2l < x1l and x2r <= x1l:
         # If house is lower than house[i]
-        if YO2 > YO1:
-            dy = YO2 - YB1
-            dx = XL1 - XR2
+        if y2l > y1l:
+            dy = y2l - y1u
+            dx = x1l - x2r
         # If house is higher than house[i]
-        elif YO2 < YO1:
-            dy = YO1 - YB2
-            dx = XL1 - XR2
+        elif y2l < y1l:
+            dy = y1l - y2u
+            dx = x1l - x2r
         distance = math.sqrt(math.pow(dx, 2) + math.pow(dy, 2))
 
     return distance
 
 
 def getcoordinates(house):
-    house.pos_X_L = house.position[0]
-    house.pos_X_R = house.position[0] + house.width
-    house.pos_Y_O = house.position[1]
-    house.pos_Y_B = house.position[1] + house.length
+    house.pos_x_l = house.position[0]
+    house.pos_x_r = house.position[0] + house.width
+    house.pos_y_l = house.position[1]
+    house.pos_y_u = house.position[1] + house.length
+
+
+def getcoordinateswater(water):
+    water.pos_x_l = water.position[0]
+    water.pos_x_r = water.position[0] + water.width
+    water.pos_y_l = water.position[1]
+    water.pos_y_u = water.position[1] + water.length
 
 
 def placehouses(numhouses):
@@ -243,6 +291,16 @@ def placehouses(numhouses):
     return houses
 
 
+def placewater(numwater, mapwater):
+    waters = []
+    i = numwater
+    while i > 0:
+        waters.append(Water(mapwater))
+        i -= 1
+
+    return waters
+
+
 def replacehouse(house, houses, numhouses, housenumber):
     # Calculations
     oldposition = house.position
@@ -278,28 +336,46 @@ def replacehouse(house, houses, numhouses, housenumber):
 
     houses[housenumber].position = house.position
     getcoordinates(houses[housenumber])
-    houses[housenumber].rect = (house.pos_X_L, house.pos_Y_O, house.width, house.length)
+    houses[housenumber].rect = (house.pos_x_l, house.pos_y_l, house.width, house.length)
     return houses
 
 
-def save(numhouses, runs, totalvalue, totaldistance, screen):
+def save(numhouses, numwaters, houses, waters, runs, totalvalue, totaldistance, screen):
+
+    # Finds existing csv files and assures no file is overwritten
     pngcount = 0
     while os.path.exists("Images\output%s.png" % pngcount):
         pngcount += 1
+    # Print visual outcome of run in image file
     outputfinal = os.path.join(SAVE_PATH_PNG, 'output' + pngcount.__str__())
     pygame.image.save(screen, outputfinal + '.png')
 
+    # Finds existing csv files and assures no file is overwritten
     csvcount = 0
-    while os.path.exists("output%s.csv" % csvcount):
+    while os.path.exists("CSV\output%s.csv" % csvcount):
         csvcount += 1
-    # outputfinal = os.path.join(SAVE_PATH_CSV, 'output' + csvcount.__str__())
+    print csvcount
+    # Print details of outcome of run in csv file
+    with open("CSV\output%s.csv" % csvcount, 'wb') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',', quotechar="'", quoting=csv.QUOTE_ALL)
+        writer.writerow(["numhouses", "runs", "totalvalue", "totaldistance"])
+        writer.writerow([numhouses, runs, totalvalue, totaldistance])
+        writer.writerow(["\n"])
+        writer.writerow(["water", "position", "width", "length", "free"])
+        for i in range(numwaters):
+            writer.writerow(["water" + i.__str__(), waters[i].position, waters[i].width, waters[i].length])
+        writer.writerow(["\n"])
+        writer.writerow(["house", "position", "width", "length", "free"])
+        for j in range(numhouses):
+            writer.writerow(["house" + j.__str__(), houses[j].position, houses[j].width, houses[j].length,
+                            houses[j].free])
+    csvfile.close()
 
-    csvstring = ['output' + TRY.__str__(), numhouses, runs, totalvalue, totaldistance]
-
+    # Print results of run in total.csv
     with open('total.csv', "a") as writefile:
         writer = csv.writer(writefile, delimiter=',', quotechar="'", lineterminator='\n', quoting=csv.QUOTE_ALL)
-        writer.writerow(csvstring)
-
+        writer.writerow([numhouses, numwaters, runs, totalvalue, totaldistance, "output%s.csv" % csvcount,
+                         "Images\output%s.png" % pngcount])
     writefile.close()
 
 
@@ -308,14 +384,14 @@ j = 0
 k = 0
 TRY = 0
 while i < NUM_TRY20:
-    run(NUM_HOUSES20, RUNS20, TRY)
+    run(NUM_HOUSES20, NUM_WATER, RUNS20, TRY)
     i += 1
     TRY += 1
 while j < NUM_TRY40:
-    run(NUM_HOUSES40, RUNS40, TRY)
+    run(NUM_HOUSES40, NUM_WATER, RUNS40, TRY)
     j += 1
     TRY += 1
 while k < NUM_TRY60:
-    run(NUM_HOUSES60, RUNS60, TRY)
+    run(NUM_HOUSES60, NUM_WATER, RUNS60, TRY)
     k += 1
     TRY += 1
